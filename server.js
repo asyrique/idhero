52a0bb5306afe321a9b303eca63fe34b6f8b6ab3
batu <ba921@nyu.edu>
2015-10-11 04:59:42 -0400

    text checking is being done

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.use(bodyParser.json());
 app.use(cors());
 */
 
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 var port = process.env.PORT || 8080;        // set our port
 
 
@@ -79,12 +84,73 @@ router.post('/', function(req, res) {
     var twiml = new twilio.TwimlResponse();
     
     textInput = parseBody(body);
-    textInput = textInput.split();
+    textInput = textInput.split(' ');
+    
 
-    if(textInput[0].toUpperCase() == "REGISTER"){
-        console.log(textInput[0]);
+    if(textInput[0].toUpperCase() == "HERO"){
+        console.log("HELP code executed");
+        twiml.message('REGISTER to create your account. NAME <FirstNameLastName> to add your name.');
+        twiml.message('DOB <Month-Day-Year> to add your date of birth. HEIGHT <#height> to add your height.');
+        twiml.message('VERIFY <#PhoneNumber> to ask for the phonenumber owner to verify you.');
+
+    } else if(textInput[0].toUpperCase() == "REGISTER"){
+        if (textInput.length == 1) {
+            twiml.message("Thank you, your account has been created. Your ID is your phone number.");
+        //var data = new Data();
+        //data.key = "name";
+        //data.data = textInput[1];
+        //console.log(data.data);
+        } else {
+            twiml.message("Thank you, your account has been created. Your ID is your phone number.");
+        }
+         
+        
+        //twiml.message('Thanks, your message of "' + body + '" was received!');
+    } else if(textInput[0].toUpperCase() == "NAME"){
+        if (textInput.length > 1) {
+        twiml.message("Your name is recorded as " + textInput.splice(1, textInput.length).join(' ') + ". This field is awaiting for verification.");
+        //var data = new Data();
+        //data.key = "name";
+        //data.data = textInput[1];
+        //console.log(data.data);
+        } else {
+            twiml.message('Please enter your NAME in the correct format: NAME "FirstNameLastName" (without the quotes)');
+        }
+
+    } else if(textInput[0].toUpperCase() == "DOB"){
+        if (textInput.length == 2 && textInput[1][2] == '-' && isNumeric(textInput[1][0]))  {
+        twiml.message("Your date of birth is recorded as " + textInput[1] + ". This field is awaiting for verification.");
+        //var data = new Data();
+        //data.key = "name";
+        //data.data = textInput[1];
+        //console.log(data.data);
+        } else {
+            twiml.message('Please enter your Date Of Birth in the correct format: DOB "MM-DD-YY" (without the quotes)');
+        }
+    } else if(textInput[0].toUpperCase() == "HEIGHT"){
+        if (textInput.length == 2 && isNumeric(textInput[1][0])) {
+        twiml.message("Your height is recorded as " + textInput[1] + ". This field is awaiting for verification.");
+        //var data = new Data();
+        //data.key = "name";
+        //data.data = textInput[1];
+        //console.log(data.data);
+        } else {
+            twiml.message('Please enter your HEIGHT in the correct format: HEIGHT "#height" (without the quotes in number format)');
+        }
+    } else if(textInput[0].toUpperCase() == "VERIFY"){
+        if (textInput.length == 2 && isNumeric(textInput[1][0])) {
+        twiml.message("You have asked for verification from this number" + textInput[1] + ". Wait for verification.");
+        //var data = new Data();
+        //data.key = "name";
+        //data.data = textInput[1];
+        //console.log(data.data);
+        } else {
+            twiml.message('Please enter in the correct format: VERIFY "#PhoneNumber" (without the quotes in number format)');
+        }
+    } else {
+    twiml.message('Input not valid. Please enter HERO to get a list of valid commands.');
     }
-    twiml.message('Thanks, your message of "' + body + '" was received!');
+    //twiml.message('Thanks, your message of "' + body + '" was received!');
 
    res.writeHead(200, {'Content-Type': 'text/xml'});
    res.end(twiml.toString());
