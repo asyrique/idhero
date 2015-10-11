9463b840a3ecac76d689da7d9220673b33394195
Asyrique AT <asyrique@gmail.com>
2015-10-11 14:26:37 -0400

    added initial side of verify

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var Users = require("../models/users");
 var twilio = require('twilio');
 var twiml = new twilio.TwimlResponse();
 var util = require("../models/util");
+var client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 function addData(req, res){
   if (req.action.key in req.user.data){
@@ -21,7 +22,29 @@ function addData(req, res){
       res.end(twiml.toString());
     }
   });
+}
 
+function verify(req, res){
+  User.findOne({"phone":req.action.data}, function(err, user){
+    if (err) {
+      twiml.message("This phone number is not within our Web of Trust (tm). Please find a friend who's an ID Hero to verify you."); // stretch suggest phone numbers
+    }
+    else {
+      req.user.verifications.push({user: user._id, fields: ["all"], state:0});
+      req.user.save(function(err, user){
+        if (err) console.log(err);
+        else {
+          client.sendMessage({
+            to: req.action.data,
+            from: "+15852964537",
+            body: req.user.phone + " has just asked you to verify them. Text CONFIRM " + req.user.phone + " to confirm you know them."
+          }, function(err, responseData){
+            if (err) console.log(err);
+          });
+        }
+      });
+    }
+  });
 }
 
 exports.process = function(req, res){
@@ -39,11 +62,9 @@ exports.process = function(req, res){
   if (req.action.verb == "upsert"){
     addData(req,res);
   }
-};
-
-exports.verify = function(req, res){
-
-
+  if (req.action.verb == "verify"){
+    verify(req,res);
+  }
 };
 
 exports.getProfile = function(req, res){
